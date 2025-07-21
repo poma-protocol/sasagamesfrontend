7177bd83e0862ab3d5c8b2fd2261ddc7f2f107bc
Roman Njoroge <roman.njoroge@njuguna.com>
2025-07-21 11:34:03 +0300

    feat: Saving operator wallet if not saved

--- src/components/BattleCardActionButton.tsx
+++ src/components/BattleCardActionButton.tsx
@@ -4,6 +4,14 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import axios from "axios";
 import { useState } from "react";
+import { Dialog, DialogClose, DialogDescription, DialogTitle, DialogTrigger } from "@radix-ui/react-dialog";
+import { DialogContent, DialogFooter, DialogHeader } from "./ui/dialog";
+import { Label } from "recharts";
+import { Input } from "./ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
 
 interface BattleCardActionButtonProps {
     id: number,
@@ -13,18 +21,41 @@ interface BattleCardActionButtonProps {
 }
 
 const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const storeOperatorAddress = z.object({
+    operatorAddress: z.string({ message: "Please enter operator address" }).regex(/^(0x)?[0-9a-fA-F]{40}$/, { message: "Operator address must be a valid ethereum address" })
+})
 
 export default function BattleCardActionButton(props: BattleCardActionButtonProps) {
     const { address } = useAccount();
     const navigate = useNavigate();
     const [loading, setLoading] = useState<boolean>(false);
+    const [enterOperatorAddress, setEnterOperatorAddress] = useState<boolean>(false);
+
+    const form = useForm<z.infer<typeof storeOperatorAddress>>({
+        resolver: zodResolver(storeOperatorAddress),
+    })
 
     async function handleButtonClick() {
         if (props.status === 'active' && props.playerCount < props.maxPlayers) {
-            console.log("I am here");
             if (address) {
                 try {
                     setLoading(true);
+
+                    // Check if operator wallet for user is stored
+                    const operatorWalletres = await axios.get(`${VITE_BACKEND_URL}/activity/operatorAddress`, {
+                        params: {
+                            activityid: props.id,
+                            userwallet: address
+                        }
+                    });
+
+                    let operatorWallet: string | null = operatorWalletres.data['operatorWallet'];
+                    if (!operatorWallet) {
+                        setLoading(false);
+                        setEnterOperatorAddress(true);
+                        return;
+                    }
+
                     const res = await axios.post(`${VITE_BACKEND_URL}/activity/join`, {
                         activity_id: props.id,
                         player_address: address,
@@ -41,9 +72,65 @@ export default function BattleCardActionButton(props: BattleCardActionButtonProp
                 toast.error("Please connect wallet to join battle");
             }
         } else {
-            console.log("I am not there", props.status, props.playerCount, props.maxPlayers);
             navigate(`/battles/${props.id}`);
         }
+    }
+
+    async function submitOperatorAddress(values: z.infer<typeof storeOperatorAddress>) {
+        try {
+            await axios.post(`${VITE_BACKEND_URL}/activity/operatorAddress`, {
+                activity_id: props.id,
+                useraddress: address,
+                operatoraddress: values.operatorAddress
+            });
+            toast.success("Operator address saved");
+            setEnterOperatorAddress(false);
+        } catch (err) {
+            console.error("Error storing operator address", err);
+            toast.error("Could not store operator address");
+        }
+    }
+
+    if (enterOperatorAddress === true) {
+        return (
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button
+                        onClick={handleButtonClick}
+                        className="w-full font-rajdhani font-semibold btn-gradient"
+                    >
+                        Enter Operator Address
+                    </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Enter Operator Address</DialogTitle>
+                    </DialogHeader>
+                    <Form {...form} >
+                        <form onSubmit={form.handleSubmit(submitOperatorAddress)} className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name="operatorAddress"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Operator Address</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="0x..." {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <DialogFooter>
+                                <DialogClose asChild>
+                                    <Button type="submit">Save</Button>
+                                </DialogClose>
+                            </DialogFooter>
+                        </form>
+                    </Form>
+                </DialogContent>
+            </Dialog>
+        );
     }
 
     return (
@@ -59,7 +146,7 @@ export default function BattleCardActionButton(props: BattleCardActionButtonProp
                     ? props.playerCount >= props.maxPlayers
                         ? "Battle Full"
                         : "Join Battle"
-                    : "View Battle": "Joining battle..."}
+                    : "View Battle" : "Joining battle..."}
         </Button>
     );
 }
